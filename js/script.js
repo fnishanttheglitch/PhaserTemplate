2918ab21772ecd2e397e2fe3f8200c0a9e511bb7
fnishanttheglitch <nishant@theglitch.in>
2016-10-12 15:42:54 +0400

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+gameWidth = 900;
+gameHeight = 600;
+
+var game = new Phaser.Game(gameWidth, gameHeight, Phaser.CANVAS, 'game', {
+  preload: preload,
+  create: create,
+  update: update
+});
+
+
+function preload(){
+
+
+
+
+}
+
+function update(){
+
+
+
+
+
+}
+
